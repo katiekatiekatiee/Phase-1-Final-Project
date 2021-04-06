3f4a42e33e5f148866904c6c0791066f52a15904
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-04-06 18:03:54 -0400

    building cli

--- lib/phase_one_cli_project/activity.rb
+++ lib/phase_one_cli_project/activity.rb
@@ -1,18 +1,25 @@
 class Activity
 
-    attr_accessor :activity, :type, :price
+    attr_accessor :activity, :type, :price, :participants
 
     @@all = []
 
-    def initialize(activity, type, price)
+    def initialize(activity, type, price, participants)
         @activity = activity
         @type = type
         @price = price
+        @participants = participants
         save
     end
 
     def save 
         @@all << self
     end
+
+    def self.all
+        @@all
+    end
+
     
+
 end
--- lib/phase_one_cli_project/cli.rb
+++ lib/phase_one_cli_project/cli.rb
@@ -1,3 +1,23 @@
-class CLI  
-    
+class CLI   
+
+    def start
+        puts "Welcome! We're so glad you made it. What should we call you? (Please enter your name)"
+        input = user_input
+        greeting(input)
+
+    end
+
+    def user_input
+        gets.strip
+    end
+
+    def greeting(name)
+        puts "Nice to meet you, #{name}! Let's find out what we're going to do today! Enter 'y' to get some ideas, or 'exit' to close our session."
+        #input = user_input
+    end
+
+
+
+
+
 end
