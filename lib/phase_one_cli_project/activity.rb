f3cc72e42c9d5567a21576b81394a52631211262
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-04-05 21:15:38 -0400

    objects are now stored in class variable

--- lib/phase_one_cli_project/activity.rb
+++ lib/phase_one_cli_project/activity.rb
@@ -2,9 +2,17 @@ class Activity
 
     attr_accessor :activity, :type, :price
 
+    @@all = []
+
     def initialize(activity, type, price)
         @activity = activity
         @type = type
         @price = price
+        save
     end
+
+    def save 
+        @@all << self
+    end
+    
 end
