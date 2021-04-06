04d997a29baec739bb8abdadfce9b110eee58b68
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-04-06 18:09:47 -0400

    menu and exit methods added to cli

--- lib/phase_one_cli_project/cli.rb
+++ lib/phase_one_cli_project/cli.rb
@@ -13,10 +13,26 @@ class CLI
 
     def greeting(name)
         puts "Nice to meet you, #{name}! Let's find out what we're going to do today! Enter 'y' to get some ideas, or 'exit' to close our session."
-        #input = user_input
+        input = user_input
     end
 
+    def menu
+        choice = user_input
+        if choice == "y"
+            #print activity list
+        elsif choice == "exit"
+            exit
+        else
+            #invalid message and rerun method to give options again
+        end
+    end
 
+    def print_activities
+    end
+
+    def exit
+        puts "I hope you found something fun to do! See you again soon!"
+    end
 
 
 
