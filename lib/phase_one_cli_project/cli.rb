a7d9d97cb283fd2e9eef5c5dbc72ab0d69ec381f
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-04-06 22:09:48 -0400

    building cli

--- lib/phase_one_cli_project/cli.rb
+++ lib/phase_one_cli_project/cli.rb
@@ -35,8 +35,23 @@ class CLI
         see_more
     end
 
+    def see_more
+        puts "Would you like to learn more about this activity and what it requires? Enter 'y' for yes, 'n' to see a different option, or 'exit' to end our session."
+        choice = user_input
+        if choice == "y"
+            #print attributes of activity
+        elsif "n"
+            print_random_activity
+        elsif "exit"
+            exit_message
+        else
+            error_message
+    
+    end
+
     def error_message
         puts "I don't recognize that entry. To continue, please enter a valid selection."
+        menu
     end
 
     def exit_message
